b6b69372c7239096b8701ddcd066e9358b823300
goindolgo <goindol@live.com>
2017-04-20 11:43:30 +0900

    added 'tell my world'

--- hello.java
+++ hello.java
@@ -1,16 +1,13 @@
 // For command line git tutorial
 // Local repository modification
-<<<<<<< HEAD
-// 3rd line local change
-=======
 // changes made on github
->>>>>>> origin/master
 class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("My name is Seungtae Kim");
 		System.out.println("Tell your world");
 		System.out.println("Edited on 'hotfix'");
+		System.out.println("Tell my world");
 	}
 }
 
